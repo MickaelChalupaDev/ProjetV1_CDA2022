b6b06ef84c86553c86df84696e6124605fca865f
Mickael Chalupa <59089788+MickaelChalupaDev@users.noreply.github.com>
2022-12-19 15:18:13 +0100

    Add files via upload

---
+++ EnI-Enchere/src/main/java/fr/eni/enchere/controllers/HelloWorld.java
@@ -0,0 +1,33 @@
+package fr.eni.enchere.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/helloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		final PrintWriter writer = response.getWriter();
+		writer.println("\n");
+		writer.println("Salut les gars !!!");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ EnI-Enchere/src/main/webapp/helloWorld.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="helloWorld" method="get">
+<h1>HELLO WORLD</h1>
+<a href="helloWorld">Click me</a>
+</form>
+</body>
+</html>
