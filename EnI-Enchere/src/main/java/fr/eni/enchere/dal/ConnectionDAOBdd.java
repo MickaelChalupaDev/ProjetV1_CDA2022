dde7aef2b7ecce4f269896c14678549aa1da1cd3
kamelEni2022 <kamel.nafkha2022@campus-eni.fr>
2022-12-22 13:51:09 +0100

    ConnectionBDD

---
+++ EnI-Enchere/src/main/java/fr/eni/enchere/dal/ConnectionDAOBdd.java
@@ -0,0 +1,30 @@
+package fr.eni.encheres.dal;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class ConnectionDAOBdd {
+
+	private static DataSource dataSource;
+
+	static {
+		Context context;
+		try {
+
+			context = new InitialContext();
+			ConnectionDAOBdd.dataSource = (DataSource) context.lookup("java:comp/env/jdbc/pool_connexion");
+
+		} catch (final Exception e) {
+			throw new RuntimeException("Impossible d'accéder à la base de Données !!!");
+		}
+
+	}
+
+	public static Connection getConnection() throws SQLException {
+		return ConnectionDAOBdd.dataSource.getConnection();
+	}
+}
