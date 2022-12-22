cc03661d884c94c3758bbba2429ae60e54b30382
kamelEni2022 <kamel.nafkha2022@campus-eni.fr>
2022-12-22 14:17:31 +0100

    Classes Enchere

--- EnI-Enchere/src/main/java/fr/eni/enchere/bll/EnchereManager.java
+++ EnI-Enchere/src/main/java/fr/eni/enchere/bll/EnchereManager.java
@@ -1,24 +1,39 @@
-package fr.eni.enchere.bll;
-
-import fr.eni.enchere.bo.Article;
-import fr.eni.enchere.bo.Categorie;
-import fr.eni.enchere.dal.DAOFactory;
-import fr.eni.enchere.dal.EnchereDAO;
+package fr.eni.encheres.bll;
 
 import java.util.List;
 
+import fr.eni.encheres.bo.Article;
+import fr.eni.encheres.bo.Categorie;
+import fr.eni.encheres.bo.Enchere;
+import fr.eni.encheres.dal.DAOFactory;
+import fr.eni.encheres.dal.EnchereDAO;
+
+
 public class EnchereManager {
-	private final EnchereDAO daoEncheres;
 	
-	public EnchereManager(EnchereDAO doaEncheres) {
-		this.daoEncheres = DAOFactory.getEnchereDAO();
+private EnchereDAO daoEncheres;
+	
+	public EnchereManager() {
+		daoEncheres= DAOFactory.getEnchereDAO();
 	}
 	
-	public List<Article> rechercherAchatEncheres(String nomArticle, Categorie categorie, int noUtilisateurEnchere, String dateEnchere){
-		return daoEncheres.rechercherAchatEncheres(nomArticle, categorie, noUtilisateurEnchere, dateEnchere);
+	/**
+	 * Recherche dans la table ARTCILES dont l'enchere ouverte (en cours) :
+	 * @param nomArticle
+	 * @param categorie
+	 * @param noUtilisateurEnchere
+	 * @param dateEnchere
+	 * @return
+	 */
+	public List<Article> rechercherToutesEnchres(String nomArticle, Categorie categorie, int noUtilisateurEnchere, String dateEnchere){
+
+		return this.daoEncheres.selectAll(nomArticle, categorie, noUtilisateurEnchere, dateEnchere);
+				
 	}
-	public List<Article> rechercherMesVentesEncheres(String nomArticle, Categorie categorie,int noUtilisateurArticle, String dateDebut, String dateEnchere){
-		return daoEncheres.rechercherMesVentesEncheres(nomArticle, categorie, noUtilisateurArticle, dateDebut, dateEnchere);
+	public List<Enchere> rechercherMesEncheres(String nomArticle, Categorie categorie,int noUtilisateurArticle, String dateDebut, String dateEnchere){
+		
+		return this.daoEncheres.selectByUser(nomArticle, categorie, noUtilisateurArticle, dateDebut, dateEnchere);
+	
 	}
 
 }
--- EnI-Enchere/src/main/java/fr/eni/enchere/dal/EnchereDAO.java
+++ EnI-Enchere/src/main/java/fr/eni/enchere/dal/EnchereDAO.java
@@ -1,12 +1,18 @@
-package fr.eni.enchere.dal;
+package fr.eni.encheres.dal;
 
 import java.util.List;
 
-import fr.eni.enchere.bo.Article;
-import fr.eni.enchere.bo.Categorie;
-import fr.eni.enchere.bo.Utilisateur;
+import fr.eni.encheres.bo.Article;
+import fr.eni.encheres.bo.Categorie;
+import fr.eni.encheres.bo.Enchere;
+
 
 public interface EnchereDAO {
-	public List<Article> rechercherAchatEncheres(String nomArticle, Categorie categorie, int noUtilisateurEnchere, String dateEnchere);
-	public List<Article> rechercherMesVentesEncheres(String nomArticle, Categorie categorie,int noUtilisateurArticle, String dateDebut, String dateEnchere);
+
+	public List<Article> selectAll(String nomArticle, Categorie categorie, int noUtilisateurEnchere, String dateEnchere);
+	public List<Article> selectByUser(String nomArticle, Categorie categorie,int noUtilisateurArticle, String dateDebut, String dateEnchere);
+	public void insert(Enchere enchere);
+	public void update(Enchere enchere);
+	
+
 }
