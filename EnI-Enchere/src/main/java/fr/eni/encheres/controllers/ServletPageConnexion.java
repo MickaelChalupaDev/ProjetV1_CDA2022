12b983add6b1bd69a6604a9b1c12b6e4fbf0606a
kamelEni2022 <kamel.nafkha2022@campus-eni.fr>
2023-01-03 17:53:48 +0100

    Page Connexion modifié pour afficher messages d'erreurs et inclure recherche par mail+pseudo

--- EnI-Enchere/src/main/java/fr/eni/encheres/controllers/ServletPageConnexion.java
+++ EnI-Enchere/src/main/java/fr/eni/encheres/controllers/ServletPageConnexion.java
@@ -1,39 +1,63 @@
 package fr.eni.encheres.controllers;
 
-import fr.eni.encheres.bll.UtilisateurManager;
-import fr.eni.encheres.bo.Utilisateur;
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-@WebServlet(name = "ServletPageConnexion", value = "/ServletPageConnexion")
+import fr.eni.encheres.bll.UtilisateurManager;
+import fr.eni.encheres.bo.Utilisateur;
+
+/**
+ * Servlet implementation class ServletPageConnexion
+ */
 public class ServletPageConnexion extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("PageConnexion.jsp").forward(request, response);
-    }
+	private static final long serialVersionUID = 1L;
+       
+   	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.getRequestDispatcher("PageConnexion.jsp").forward(request, response);
+	}
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
-        String pseudo= null;
-        String motDePasse=null;
-        UtilisateurManager uMgr= new UtilisateurManager();
-        Utilisateur user = new Utilisateur();
-        pseudo= request.getParameter("pseudo");
-        motDePasse= request.getParameter("motDePasse");
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		
+		String pseudo= null;
+		String motDePasse=null;
+		UtilisateurManager uMgr= new UtilisateurManager();
+		Utilisateur user = new Utilisateur();
+		pseudo= request.getParameter("pseudo");
+		motDePasse= request.getParameter("motDePasse");
+		 
+		user=uMgr.lireUtilisateur(pseudo);
+		if (user!=null) {
+		if (user.getMotDePasse().equals(motDePasse)) {
+		
+			session.setAttribute("utilisateur", user);
+			request.getRequestDispatcher("Accueil").forward(request, response);
+			return;
+		} else {
+			
+			request.setAttribute("messageErreur", " Mot de passe incorrect");
+			request.getRequestDispatcher("PageConnexion.jsp").forward(request, response);
+			return;
+		} }else {
+			request.setAttribute("messageErreur", "Identifiant incorrect");
+			request.getRequestDispatcher("PageConnexion.jsp").forward(request, response);
+		}
+		
+		
+		
+	}
 
-        user=uMgr.lireUtilisateur(pseudo);
-
-        if (user.getMotDePasse().equals(motDePasse)) {
-            session.setAttribute("utilisateur", user);
-            request.getRequestDispatcher("Accueil").forward(request, response);
-        } else {
-            user=null;
-            session.setAttribute("utilisateur", user);
-            request.getRequestDispatcher("Accueil").forward(request, response);
-        }
-    }
 }
